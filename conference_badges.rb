08cc630d42641d6b1e3d3f4a7e4f84e77076f0af
Elaina <elainaseki@gmail.com>
2019-02-14 00:12:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  response = []
+  attendees.each do|name|
+  response << "Hello, my name is #{name}."
+  end
+  return response
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  i = 0
+  while i < attendees.length
+  name = attendees[i]
+  room_assignments << "Hello, #{name}! You'll be assigned to room #{i+1}!"
+  i += 1
+  end
+  return room_assignments
+end
 # Write your code here.
